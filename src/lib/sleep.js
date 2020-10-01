4433cbb4082622fcef6b75e035cc166fb0dfe866
Zhenyu Lin <zhenyu.lyn@gmail.com>
2020-10-01 16:22:19 +0100

    docs: cover lib

--- src/lib/sleep.js
+++ src/lib/sleep.js
@@ -1,3 +1,9 @@
+/**
+ * sleep for n ms before resolving the Promise
+ *
+ * @param  {Number} time duration in ms
+ * @returns {Promise} a Promise for async/await control flow
+ */
 const sleep = (time) =>
   new Promise((resolve) => {
     setTimeout(() => {
